d009c72e718a3e95dc0f4acd31db31ec6be88615
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-11-22 18:39:44 -0300

    Support tweeting status

--- tt.sh
+++ tt.sh
@@ -4,10 +4,10 @@ while true; do
     if [ "$(jq -r '.daipeg' status.json)" = "false" ]; then
        if [ ! -f daipeg.broken ]; then
           touch daipeg.broken
-          # tweet "Dai USD peg is temporarily broken! 1 DAI = $(jq -r '.daiusd' status.json) USD"
+          echo "Dai USD peg is temporarily broken! 1 DAI = $(jq -r '.daiusd' status.json) USD" | ./tweet.sh post
 fi
        else
-          if [ -f daipeg.broken ]; then echo "tweet Dai USD peg has been restored! 1 DAI = $(jq -r '.daiusd' status.json) USD"; rm -f daipeg.broken; fi
+          if [ -f daipeg.broken ]; then echo "Dai USD peg has been restored! 1 DAI = $(jq -r '.daiusd' status.json) USD" | ./tweet.sh post; rm -f daipeg.broken; fi
 fi
     sleep 300
 done
