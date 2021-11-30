c490c83c23cd732fd1a85a99fc36e195985a1520
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-11-30 17:37:43 -0300

    Ready for server deploy

--- status.sh
+++ status.sh
@@ -3,16 +3,16 @@
 while true; do
     daiusd="$(curl -s "https://api.coingecko.com/api/v3/simple/price?ids=dai&vs_currencies=usd" | jq -r '.dai.usd')"
     if [ ${daiusd%%.*} -ge "1" ]; then
-       contents="$(jq '.daipeg = true' status.json)" && \
-       echo "${contents}" > status.json
+       contents="$(jq '.daipeg = true' /var/www/html/status.json)" && \
+       echo "${contents}" > /var/www/html/status.json
        else
-          contents="$(jq '.daipeg = false' status.json)" && \
-          echo "${contents}" > status.json
+          contents="$(jq '.daipeg = false' /var/www/html/status.json)" && \
+          echo "${contents}" > /var/www/html/status.json
 fi
-    contents="$(jq ".daiusd = \"$daiusd\"" status.json)" && \
-    echo "${contents}" > status.json
+    contents="$(jq ".daiusd = \"$daiusd\"" /var/www/html/status.json)" && \
+    echo "${contents}" > /var/www/html/status.json
     recentlyupdatedat=$(date +"%s")
-    contents="$(jq ".lastupdate = \"$recentlyupdatedat\"" status.json)" && \
-    echo "${contents}" > status.json
+    contents="$(jq ".lastupdate = \"$recentlyupdatedat\"" /var/www/html/status.json)" && \
+    echo "${contents}" > /var/www/html/status.json
     sleep 300
 done
--- tt.sh
+++ tt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
 while true; do
-    if [ "$(jq -r '.daipeg' status.json)" = "false" ]; then
+    if [ "$(jq -r '.daipeg' /var/www/html/status.json)" = "false" ]; then
        if [ ! -f daipeg.broken ]; then
           touch daipeg.broken
-          echo "Dai USD peg is temporarily broken! 1 DAI = $(jq -r '.daiusd' status.json) USD" | ./tweet.sh post
+          echo "Dai USD peg is temporarily broken! 1 DAI = $(jq -r '.daiusd' /var/www/html/status.json) USD" | ./tweet.sh post
 fi
        else
-          if [ -f daipeg.broken ]; then echo "Dai USD peg has been restored! 1 DAI = $(jq -r '.daiusd' status.json) USD" | ./tweet.sh post; rm -f daipeg.broken; fi
+          if [ -f daipeg.broken ]; then echo "Dai USD peg has been restored! 1 DAI = $(jq -r '.daiusd' /var/www/html/status.json) USD" | ./tweet.sh post; rm -f daipeg.broken; fi
 fi
     sleep 300
 done
