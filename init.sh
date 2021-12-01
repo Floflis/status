6d9109deb5429c01c45b7ec1f81f3f9557c4fe6c
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-12-01 15:44:47 -0300

    offload init.sh and use separate script to update the site

--- init.sh
+++ init.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
+sh ./update-site.sh
+
 sudo apt update
 
-sudo apt install openssl nginx -y
-cp site/* /var/www/html
-rm -rf site
-rm -f /var/www/html/index.nginx-debian.html
-sudo ufw allow 'Nginx HTTP'
-
 sudo apt install curl jq nkf -y
-
-sudo chown -R $USER:$USER /var/www
 
 cat > /var/www/html/status.json << ENDOFFILE
 {
---
+++ update-site.sh
@@ -0,0 +1,7 @@
+sudo apt install openssl nginx -y
+cp site/* /var/www/html
+rm -rf site
+rm -f /var/www/html/index.nginx-debian.html
+sudo ufw allow 'Nginx HTTP'
+
+sudo chown -R $USER:$USER /var/www
