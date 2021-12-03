ca4453b574bbe5b03890b5c118aee97f1df956ed
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-12-02 22:38:40 -0300

    Updates

--- status.sh
+++ status.sh
@@ -5,26 +5,22 @@ while true; do
     if [ ${daiusd%%.*} -ge "1" ]; then
        contents="$(jq '.daipeg = true' /var/www/html/status.json)" && \
        echo "${contents}" > /var/www/html/status.json
-#       sed -i -e 's|<element id="daipeg">\([0-9]\{0,\}\)</element>|<element id="daipeg">'"✅ Yes"'</element>|g' /var/www/html/index.html
        contents="$(mlq '#daipeg = "Yes"' /var/www/html/index.html)" && \
        echo "${contents}" > /var/www/html/index.html
        else
           contents="$(jq '.daipeg = false' /var/www/html/status.json)" && \
           echo "${contents}" > /var/www/html/status.json
-#          sed -i -e 's|<element id="daipeg">\([0-9]\{0,\}\)</element>|<element id="daipeg">'"❌ No"'</element>|g' /var/www/html/index.html
           contents="$(mlq '#daipeg = "No"' /var/www/html/index.html)" && \
           echo "${contents}" > /var/www/html/index.html
 fi
     contents="$(jq ".daiusd = \"$daiusd\"" /var/www/html/status.json)" && \
     echo "${contents}" > /var/www/html/status.json
+    contents="$(mlq "#daiusd = \"${daiusd}\"" /var/www/html/index.html)" && \
+    echo "${contents}" > /var/www/html/index.html
     recentlyupdatedat=$(date +"%s")
     contents="$(jq ".lastupdate = \"$recentlyupdatedat\"" /var/www/html/status.json)" && \
     echo "${contents}" > /var/www/html/status.json
-#    sed -i -e 's|<element id="daiusd">\([0-9]\{0,\}\)</element>|<element id="daiusd">'"${daiusd}"'</element>|g' /var/www/html/index.html
-    contents="$(mlq "#daiusd = \"${daiusd}\"" /var/www/html/index.html)" && \
-    echo "${contents}" > /var/www/html/index.html
     contents=$(jq -r '.lastupdate' /var/www/html/status.json)
-#    sed -i -e 's|<element id="lastupdate">\([0-9]\{0,\}\)</element>|<element id="lastupdate">'"$(date -d @${contents})"'</element>|g' /var/www/html/index.html
     contentstwo="$(mlq "#lastupdate = \"${contents}\"" /var/www/html/index.html)" && \
     echo "${contentstwo}" > /var/www/html/index.html
     sleep 300
