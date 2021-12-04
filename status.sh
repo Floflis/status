66da48e9a097edd0668ef59af2513dee28a86d24
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-12-04 15:31:51 -0300

    Human-readable timestamp; better source indentation; fixes

--- status.sh
+++ status.sh
@@ -5,23 +5,26 @@ while true; do
     if [ ${daiusd%%.*} -ge "1" ]; then
        contents="$(jq '.daipeg = true' /var/www/html/status.json)" && \
        echo "${contents}" > /var/www/html/status.json
-       contents="$(mlq '#daipeg = "Yes"' /var/www/html/index.html)" && \
+       contents="$(mlq '#daipeg = "✅ Yes"' /var/www/html/index.html)" && \
        echo "${contents}" > /var/www/html/index.html
        else
           contents="$(jq '.daipeg = false' /var/www/html/status.json)" && \
           echo "${contents}" > /var/www/html/status.json
-          contents="$(mlq '#daipeg = "No"' /var/www/html/index.html)" && \
+          contents="$(mlq '#daipeg = "❌ No"' /var/www/html/index.html)" && \
           echo "${contents}" > /var/www/html/index.html
 fi
     contents="$(jq ".daiusd = \"$daiusd\"" /var/www/html/status.json)" && \
     echo "${contents}" > /var/www/html/status.json
-    contents="$(mlq "#daiusd = \"${daiusd}\"" /var/www/html/index.html)" && \
+    contents="$(mlq "#daiusd = \"$daiusd\"" /var/www/html/index.html)" && \
     echo "${contents}" > /var/www/html/index.html
+
     recentlyupdatedat=$(date +"%s")
     contents="$(jq ".lastupdate = \"$recentlyupdatedat\"" /var/www/html/status.json)" && \
     echo "${contents}" > /var/www/html/status.json
+
     contents=$(jq -r '.lastupdate' /var/www/html/status.json)
-    contentstwo="$(mlq "#lastupdate = \"${contents}\"" /var/www/html/index.html)" && \
-    echo "${contentstwo}" > /var/www/html/index.html
+    contentstwo=$(date -d @${contents})
+    contentsthree="$(mlq "#lastupdate = \"${contentstwo}\"" /var/www/html/index.html)" && \
+    echo "${contentsthree}" > /var/www/html/index.html
     sleep 300
 done
