77e20dbb7f692ee4c350c5aad22799b0563acf14
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-10 14:47:04 -0300

    old updates for update-site.sh

--- update-site.sh
+++ update-site.sh
@@ -4,6 +4,19 @@ sudo apt install openssl nginx -y
 cp site/* /var/www/html
 rm -rf site
 rm -f /var/www/html/index.nginx-debian.html
+#sudo ufw app list
 sudo ufw allow 'Nginx HTTP'
+#sudo ufw allow 'Nginx HTTPS'
+#ufw allow OpenSSH
+#ufw enable
+#sudo ufw status
+#systemctl status nginx
 
 sudo chown -R $USER:$USER /var/www
+
+#ufw status
+#ufw allow OpenSSH
+#ufw enable
+
+#adduser  --gecos GECOS
+#usermod -aG sudo 
